73bbd4c79368051d13f776e3eb18fee6cf453970
Kristian Glass <git@doismellburning.co.uk>
2013-08-08 14:16:58 +0100

    Consistent pluralisation

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,11 +3,11 @@ class python::params {
   case $::operatingsystem {
     'CentOS', 'Fedora', 'Scientific', 'RedHat', 'Amazon': {
       $package = 'python'
-      $development_package = ['python-devel']
+      $development_packages = ['python-devel']
     }
     'Debian', 'Ubuntu': {
       $package = 'python'
-      $development_package = ['python-dev', 'python-pip']
+      $development_packages = ['python-dev', 'python-pip']
     }
     default: {
       fail("\"${module_name}\" provides no package default value
