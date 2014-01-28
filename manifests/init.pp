2a24736e74728b0e2c7ec8399e2149b96cafb637
Paul Houghton <paul4hough@gmail.com>
2014-01-28 10:22:26 -0700

    WIP - Add pip support to centos

--- manifests/init.pp
+++ manifests/init.pp
@@ -59,6 +59,34 @@ class python {
                         'python3-tools',
                         'python3-setuptools',
                         ]
+        exec { 'install centos pip-3.3' :
+          command => shellquote(
+            '/usr/bin/env',
+            'curl',
+            'https://raw.github.com/pypa/pip/master/contrib/get-pip.py',
+            '|',
+            'python3'),
+          creates => '/usr/bin/pip-3.3',
+          require => Package['python3-setuptools'],
+        }->
+        file { '/usr/bin/pip3' :
+          ensure => 'link',
+          target => '/usr/bin/pip-3.3'
+        }
+        exec { 'install centos pip-2.7' :
+          command => shellquote(
+            '/usr/bin/env',
+            'curl',
+            'https://raw.github.com/pypa/pip/master/contrib/get-pip.py',
+            '|',
+            'python27'),
+          creates => '/usr/bin/pip-2.7',
+          require => Package['python27-setuptools'],
+        }->
+        file { '/usr/bin/pip27' :
+          ensure => 'link',
+          target => '/usr/bin/pip-2.7'
+        }
       } else {
         $provider = undef
         $py_packages = ['python27',
